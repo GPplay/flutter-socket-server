487963b7517ebf292f540bfd2da29dc174c6b075
GPplay <gybramdali2002@hotmail.com>
2024-08-01 15:09:10 -0500

    emitir eventos de la app de flutter a el backend y dispositivos conectados a el puerto

--- sockets/socket.js
+++ sockets/socket.js
@@ -16,5 +16,11 @@ io.on('connection', client => {
         io.emit( 'mensaje', {admin: 'Gybram es el admin =D'});
     })
 
+    client.on('emitir-mensaje', (payload)=> {
+        console.log(payload);
+        //io.emit('nuevo-mensaje', payload); //emite a todos los clientes conectados
+        client.broadcast.emit('nuevo-mensaje', payload); //emite a todos menos al cliente que esta mandando el mensaje
+    })
+
 
 });
